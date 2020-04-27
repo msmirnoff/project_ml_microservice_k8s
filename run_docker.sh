8e0e40174b833955fea982a624ebc1aa5d7ac297
Michael Smirnoff <michael@michaelsmirnoff.com>
2020-04-28 00:11:53 +0930

    Script building image and running container

--- run_docker.sh
+++ run_docker.sh
@@ -2,11 +2,18 @@
 
 ## Complete the following steps to get Docker running locally
 
+name="ml-app"
+tag="dev"
+dockerhost="192.168.99.100"
+
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag "$name":"$tag" .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run --interactive --tty --publish "$dockerhost":8000:80/tcp "$name":"$tag"
