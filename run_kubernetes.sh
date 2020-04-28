e12ba4ec48dbc12675a5bb5fbada24c17f5418a1
Michael Smirnoff <michael@michaelsmirnoff.com>
2020-04-28 19:24:13 +0930

    Complete kubernetes container launch script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="igms/ml-app"
+container="ml-app"
+tag="v1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run "$container" --image="$dockerpath":"$tag" --port=80 --labels app="$container"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward "$container" 8000:80
 
