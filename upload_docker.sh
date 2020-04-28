8cf26e8d42069f2b137b4cf5278dfc6c642feeb5
Michael Smirnoff <michael@michaelsmirnoff.com>
2020-04-28 18:48:24 +0930

    Complete script to tag and upload to DockerHub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="igms/ml-app"
+tag="v1"
+built_image="ml-app:dev"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=igms
+docker tag "$built_image" "$dockerpath":"$tag"
 
 # Step 3:
 # Push image to a docker repository
+docker push "$dockerpath":"$tag"
